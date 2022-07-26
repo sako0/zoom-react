2a79b9d422b13b96357e72786d7672ae4684ef44
sako0 <y.m1911a1.0921@gmail.com>
2022-07-26 17:20:49 +0900

    zoomのtokenを表示させる様に修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,28 @@ import { useEffect, useState } from 'react'
 import LoginButton from '../components/LoginButton'
 import LogoutButton from '../components/LogoutButton'
 
+const decodeJwt = (token: string) => {
+  const base64Url = token.split('.')[1]
+  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/')
+  return JSON.parse(decodeURIComponent(escape(window.atob(base64))))
+}
+
 const Home = () => {
   const [token, setToken] = useState<string>('')
+  const [zoomToken, setZoomToken] = useState<string>('')
+  const [zooomRefreshToken, setZooomRefreshToken] = useState<string>('')
   const { isAuthenticated, user, getAccessTokenSilently } = useAuth0()
 
   useEffect(() => {
     if (isAuthenticated) {
       getAccessTokenSilently().then((res) => {
         setToken(res)
+
+        setZoomToken(decodeJwt(res)['https://oauth.zoom.us/token'].accessToken)
+        setZooomRefreshToken(
+          decodeJwt(res)['https://oauth.zoom.us/token'].refreshToken
+        )
+
         const requestOptions = {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
@@ -28,11 +42,13 @@ const Home = () => {
       ) : (
         <>
           <img src={user?.picture} className="rounded-3xl mt-2 mx-auto" />
-          <p
-            className="font-thin my-4 mx-10"
-            style={{ overflowWrap: 'break-word' }}
-          >
-            {token}
+          <p className="fant-bold mt-4 mb-1 mx-auto">ZoomのToken</p>
+          <p className="font-thin mx-10" style={{ overflowWrap: 'break-word' }}>
+            {zoomToken}
+          </p>
+          <p className="fant-bold mt-4 mb-1 mx-auto">ZoomのRefreshToken</p>
+          <p className="font-thin mx-10" style={{ overflowWrap: 'break-word' }}>
+            {zooomRefreshToken}
           </p>
           <div className="mx-auto">
             <LogoutButton />
